bbadae50448984df2fecbb3d0b311e68e7822bc8
Ivan Kovic <ivan.kovic@seekandhit.com>
2017-06-30 10:59:48 +0200

    Project setup

--- src/App.js
+++ src/App.js
@@ -1,19 +1,66 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import * as firebase from 'firebase';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom';
+
+import Order from './features/Order';
+
 import './App.css';
 
+const Call = () => (
+  <div>
+    <h2>Call</h2>
+  </div>
+);
+
+const Adverbs = () => (
+  <div>
+    <h2>Adverbs</h2>
+  </div>
+);
+
+
 class App extends Component {
+
+  componentDidMount() {
+    const config = {
+      apiKey: "AIzaSyB-00Z6-M47FA2gTsT_QS0QyNbWMOhL1cQ",
+      authDomain: "bilipivac-f2b81.firebaseapp.com",
+      databaseURL: "https://bilipivac-f2b81.firebaseio.com",
+      projectId: "bilipivac-f2b81",
+      storageBucket: "bilipivac-f2b81.appspot.com",
+      messagingSenderId: "452297064967"
+    };
+
+    firebase.initializeApp(config);
+
+    const userRef = firebase.database().ref('users');
+
+    userRef.on('value', function (snapshot) {
+      console.log(snapshot.val());
+    });
+  }
+
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+      <Router>
+        <div>
+          <ul>
+            <li><Link to="/">Order</Link></li>
+            <li><Link to="/call">Call</Link></li>
+            <li><Link to="/adverbs">Adverbs</Link></li>
+          </ul>
+
+          <hr />
+
+          <Route exact path="/" component={Order} />
+          <Route path="/call" component={Call} />
+          <Route path="/adverbs" component={Adverbs} />
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      </Router>
     );
   }
 }
---
+++ src/features/Order/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Order = () => (
+  <div>
+    <h2>Order</h2>
+  </div>
+);
+
+export default Order;
